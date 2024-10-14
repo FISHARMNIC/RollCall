0e56f345016d7610b04d192f004112bd8ccfeb55
Nico Q <73864341+FISHARMNIC@users.noreply.github.com>
2024-10-14 00:36:35 -0600

    removed old code

--- src/lib/rollcall.c
+++ src/lib/rollcall.c
@@ -4,31 +4,6 @@
 Linked_t *Roster = 0;
 
 const int SIZE_ROSTER_AND_ENTRY = (sizeof(roster_entry_t) + sizeof(roster_entry_t *));
-/*
-void *rc_allocate(int size_bytes, int restricted)
-{
-    // Note, here using malloc which also stores size.
-    // In compiler use mmap2
-
-    roster_entry_t *roster_entry = malloc(sizeof(roster_entry_t));
-    described_buffer_t *described_buffer = malloc(sizeof(roster_entry_t *) + size_bytes);
-    assert(roster_entry != 0);
-    assert(described_buffer != 0);
-
-    described_buffer->entry_reference = roster_entry;
-
-    roster_entry->owner = 0;
-    roster_entry->restricted = restricted;
-    roster_entry->size = size_bytes;
-    roster_entry->pointer = &(described_buffer->data);
-
-    linked_add(&Roster, roster_entry);
-
-    printf("Allocated Roster[%i] {%i} @%p\n", linked_getSize(Roster), size_bytes, &(described_buffer->data));
-
-    return roster_entry->pointer;
-}
-*/
 
 void *rc_allocate(int size_bytes, int restricted)
 {
